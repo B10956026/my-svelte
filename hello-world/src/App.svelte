ba8b5c09b2943b6dc8aeb64f0c0e5fbc0573f924
B10956026 <84659977+B10956026@users.noreply.github.com>
2024-01-20 17:42:59 +0800

    Binding Classes

--- hello-world/src/App.svelte
+++ hello-world/src/App.svelte
@@ -4,9 +4,15 @@
 	const hack = `<a href="#" onclick="alert('You have been hacked!')">Win a prize!'<a/>`
 	const id = 'heading'
 	const disabled = false
+	const status = 'success'
+	const promoted = true
 </script>
 
 <main>
+	<h2 class="underline">Underlined Text</h2>
+	<h2 class={status}>Status</h2>
+	<h2 class:promoted>Movie Title</h2>
+	
 	<h1>B10956026 {name}</h1>
 	<div>{@html channel}</div>
 	<div>{@html hack}</div>
@@ -15,6 +21,22 @@
 </main>
 
 <style>
+	.underline {
+		text-decoration: underline;
+	}
+
+	.danger {
+		color:red;
+	}
+
+	.success {
+		color:olive;
+	}
+
+	.promoted {
+		font-style: italic;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
