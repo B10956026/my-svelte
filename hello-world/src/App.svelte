0fe6f90a6e51f1297cd16b076d2857a8cb43292b
B10956026 <84659977+B10956026@users.noreply.github.com>
2024-01-20 18:13:54 +0800

    update file

--- hello-world/src/App.svelte
+++ hello-world/src/App.svelte
@@ -1,17 +1,17 @@
 <script>
 	const name = '謝東翰'
 	const channel = '<b>國立屏東科技大學 資訊管理系</b>'
-	const hack = `<a href="#" onclick="alert('You have been hacked!')">Win a prize!'<a/>`
+	const hack = `<a href="#" onclick="alert('您已被黑客攻擊！')">恭喜中獎!'<a/>`
 	const id = 'heading'
-	const disabled = false
+	const disabled = true
 	const status = 'success'
 	const promoted = true
 </script>
 
 <main>
-	<h2 class="underline">Underlined Text</h2>
-	<h2 class={status}>Status</h2>
-	<h2 class:promoted>Movie Title</h2>
+	<h2 class="underline">桜巫女（さくらみこ）</h2>
+	<h2 class={status}>狀態</h2>
+	<h2 class:promoted>Hololive Production</h2>
 	
 	<h1>B10956026 {name}</h1>
 	<div>{@html channel}</div>
